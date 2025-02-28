8a91c3f995ade1670c02b0d9704858d6e40857dc
Priyanshu-Kumaar <priyanshukumar082004@gmail.com>
2025-02-28 19:40:25 +0530

    Saved Jobs Page

--- src/pages/saved-jobs.jsx
+++ src/pages/saved-jobs.jsx
@@ -30,6 +30,7 @@ const SavedJobs = () => {
       <h1 className=" mx-auto gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8 ">
         Saved Jobs
       </h1>
+      
 
       {loadingSavedJobs === false && (
         <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
