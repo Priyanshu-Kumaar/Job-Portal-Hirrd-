7fb2ce770061109b38a5138472c13d98f1f49485
Priyanshu-Kumaar <priyanshukumar082004@gmail.com>
2025-02-28 19:23:02 +0530

    Page Layout

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import { useUser } from "@clerk/clerk-react";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
