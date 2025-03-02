312d9bb106f7c20cbbf3df50751e1e20a40ade66
Priyanshu-Kumaar <priyanshukumar082004@gmail.com>
2025-03-02 14:04:09 +0530

    Changes in CSS

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -4,8 +4,8 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div className="md:pl-10 ">
-      <div className="grid-background  "></div>
+    <div className="md:pl-10 pl-12">
+      <div className="grid-background   "></div>
         <Header />
         <Outlet />
     </div>
